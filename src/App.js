261765c36c96d58a2992558a34ff084e6e81aad0
NIRANJANA1212 <160489867+NIRANJANA1212@users.noreply.github.com>
2024-02-27 10:40:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './componenets/Login';
+// import logo from './logo.svg';
+// import './App.css';
+// import Login from './componenets/Login';
 import Navbar from './componenets/Navbar';
-import StateBasics from './componenets/StateBasics';
-import Counter from './componenets/Counter';
-import Signup from './componenets/Signup';
-// import Mapping from './componenets/Mapping';
-import Tables from './componenets/Tables';
-import Carda from './componenets/Carda';
-import Texts from './componenets/Texts';
-import Grida from './componenets/Grida';
-import Api from './componenets/Api';
+// import StateBasics from './componenets/StateBasics';
+// import Counter from './componenets/Counter';
+// import Signup from './componenets/Signup';
+// // import Mapping from './componenets/Mapping';
+// import Tables from './componenets/Tables';
+// import Carda from './componenets/Carda';
+// import Texts from './componenets/Texts';
+// import Grida from './componenets/Grida';
+// import Api from './componenets/Api';
 import Useeffect from './componenets/UseEffect';
 import { Routes,Route } from 'react-router-dom';
-function App() {
+function Blog() {
   return (
-    <div className="App">
+    <div className="Blog">
       <Navbar/>
       <Routes>
         <Route path='/' element={<Login/>}></Route>
         <Route path='/sign' element={<Signup/>}></Route>
         <Route path='/count' element={<Counter/>}></Route>
-        <Route path='/a' element={<Tables/>}></Route>
-        <Route path='/b' element={<Texts/>}></Route>
-        <Route path='/c' element={<Api/>}></Route>
-        <Route path='/d' element={<Grida/>}></Route>
-        <Route path='/e' element={<Carda/>}></Route>
+        // <Route path='/a' element={<Tables/>}></Route>
+        // <Route path='/b' element={<Texts/>}></Route>
+        // <Route path='/c' element={<Api/>}></Route>
+        // <Route path='/d' element={<Grida/>}></Route>
+        // <Route path='/e' element={<Carda/>}></Route>
 
 
 
